dcd63a47391e388891b6ec611368abee39392fd6
Savser91 <savser91@gmail.com>
2022-04-04 21:29:06 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+import server.Server;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final var validPaths = List.of("/index.html", "/spring.svg",
+                "/spring.png", "/resources.html", "/styles.css", "/app.js",
+                "/links.html", "/forms.html", "/classic.html", "/events.html",
+                "/events.js");
+
+    }
+}
